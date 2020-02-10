dcfddd8f526e3a7ad540757f36748df1e8369678
Krzysztof Grzywocz <kgrzywocz@wp.pl>
2020-02-10 22:20:37 +0100

    added example config

--- testlib/build.rs
+++ testlib/build.rs
@@ -7,6 +7,8 @@ fn main() {
     let src = outdir.join("app.exe");
     outdir.pop();
     let dst = outdir.join("League of Legends.exe");
+    // Riot Games\League of Legends\Game
+    // Riot Games\League of Legends\Config\game.cfg
 
     println!("Moving {:?} -> {:?}", src, dst);
     std::fs::rename(src, dst).expect("Unable to move test app");
