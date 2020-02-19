2b49f2a93531a5672eb0c0b883a1afd154afdf39
Krzysztof Grzywocz <kgrzywocz@wp.pl>
2020-02-19 13:10:18 +0100

    proces helper refactor  tlhelp32.h->psapi.h

--- lol-lib/cpp/src/ProcessHelper.cpp
+++ lol-lib/cpp/src/ProcessHelper.cpp
@@ -1,37 +1,20 @@
 #include "ProcessHelper.hpp"
 
 #include <windows.h>
-#include <tlhelp32.h>
 #include <psapi.h>
 
-DWORD getProcessId(const std::string &processName)
+
+inline bool ends_with(std::string const & where, std::string const & what)
 {
-    PROCESSENTRY32 entry;
-
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-
-    if (Process32First(snapshot, &entry))
-        while (Process32Next(snapshot, &entry))
-        {
-            if (!strcmp(entry.szExeFile, processName.c_str()))
-            {
-                CloseHandle(snapshot);
-                return entry.th32ProcessID;
-            }
-        }
-    CloseHandle(snapshot);
-    return NULL;
+    if (what.size() > where.size())
+        return false;
+    else
+        return std::equal(what.rbegin(), what.rend(), where.rbegin());
 }
 
-bool isProcessRunning(const std::string &processName)
-{
-    return getProcessId(processName) != NULL;
-}
-
-std::string getProcessRunningPath(const std::string &exeName)
+std::string getProcessFile(DWORD pid)
 {
     TCHAR filename[MAX_PATH] = "";
-    auto pid = getProcessId(exeName);
 
     HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
     if (processHandle != NULL)
@@ -40,4 +23,25 @@ std::string getProcessRunningPath(const std::string &exeName)
         CloseHandle(processHandle);
     }
     return filename;
+}
+
+std::string getProcessRunningPath(const std::string &exeName)
+{
+    DWORD pids[1024], pidsBytes;
+    if ( !EnumProcesses( pids, sizeof(pids), &pidsBytes ) )
+        return "";
+    auto nPids = pidsBytes / sizeof(pidsBytes);
+
+    while(nPids--)
+    {
+        auto filePath = getProcessFile(pids[nPids]);
+        if (ends_with(filePath, exeName))
+            return filePath;
+    }
+    return "";
+}
+
+bool isProcessRunning(const std::string &processName)
+{
+    return getProcessRunningPath(processName) != "";
 }
