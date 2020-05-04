43e204d282a94a48b51bdf3f26ba1917001e6244
Krzysztof Grzywocz <kgrzywocz@wp.pl>
2020-05-04 17:06:13 +0200

    backend rust wraper

--- lol-lib/backend_interface/lib.rs
+++ lol-lib/backend_interface/lib.rs
@@ -51,3 +51,79 @@ extern "C" {
     pub fn lollib_backend_pixelRect_getWidth(rect: *const BackendPixelRect) -> i32;
     pub fn lollib_backend_pixelRect_getColor(rect: *const BackendPixelRect, row: i32, column: i32) -> BackendColor;
 }
+
+struct Backend{
+    backend_screen_analyzer: *mut BackendScreenAnalyzer,
+}
+impl Backend{
+    fn new(analyze_function: FrontendAnalysisFunction)->Self{
+        unsafe{
+            let backend_screen_analyzer=lollib_backend_createBackendScreenAnalyzer(analyze_function);
+            if std::ptr::null() == backend_screen_analyzer
+            {
+                panic!("lollib_backend not correctly initialized!");
+            }
+            Self{backend_screen_analyzer}
+        }
+    }
+
+    pub fn get_mode(&self) -> BackendScreenResolution{
+        unsafe{
+            lollib_backend_getMode(self.backend_screen_analyzer)
+        }
+    }
+    pub fn has_mode_changed(&self) -> i32{
+        unsafe{
+            lollib_backend_hasModeChanged(self.backend_screen_analyzer)
+        }
+    }
+    pub fn set_capture_rect(&self, capture_rect: *const BackendCaptureRect){
+        unsafe{
+            lollib_backend_setCaptureRect(self.backend_screen_analyzer, capture_rect);
+        }
+    }
+    pub fn analyze_screenshot(&self) -> LolStats{
+        unsafe{
+            lollib_backend_analyzeScreenshot(self.backend_screen_analyzer)
+        }
+    }
+}
+impl Drop for Backend{
+    fn drop(&mut self){
+        unsafe{
+            lollib_backend_destroyBackendScreenAnalyzer(self.backend_screen_analyzer)
+        };
+    }
+}
+
+struct PixelRectWraper
+{
+    rect: *const BackendPixelRect,
+}
+
+impl PixelRectWraper{
+    fn new(rect: *const BackendPixelRect) ->Self
+    {
+        if std::ptr::null() == rect
+        {
+            panic!("lollib_backend BackendPixelRect not correctly initialized!");
+        }
+        Self{rect}
+    }
+
+    pub fn get_hight(&self) -> i32{
+        unsafe{
+            lollib_backend_pixelRect_getHight(self.rect)
+        }
+    }
+    pub fn get_width(&self) -> i32{
+        unsafe{
+            lollib_backend_pixelRect_getWidth(self.rect)
+        }
+    }
+    pub fn get_color(&self, row: i32, column: i32) -> BackendColor{
+        unsafe{
+            lollib_backend_pixelRect_getColor(self.rect, row, column)
+        }
+    }
+}
